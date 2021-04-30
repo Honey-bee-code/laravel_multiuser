8a133dc5fcb4a1a7048f57df1c405e222c6dc778
Honey-bee-code <hanifinida@gmail.com>
2021-04-30 19:35:51 +0800

    UserSeeder

--- database/seeders/CreateUserSeeder.php
+++ database/seeders/CreateUserSeeder.php
@@ -14,5 +14,24 @@ class CreateUserSeeder extends Seeder
     public function run()
     {
         //
+        $user = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@honey.com',
+                'is_admin' => '1',
+                'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@honey.com',
+                'is_admin' => '0',
+                'password' => bcrypt('123456'),
+            ],
+        ];
+
+        foreach ($user as $key => $value) {
+            # code...
+            User::create($value);
+        }
     }
 }
