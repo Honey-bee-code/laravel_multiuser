ae5d9955fc9756ea83f561225621cdbe5aff7957
Honey-bee-code <hanifinida@gmail.com>
2021-04-30 19:26:35 +0800

    UserSeeder

---
+++ database/seeders/CreateUserSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CreateUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
